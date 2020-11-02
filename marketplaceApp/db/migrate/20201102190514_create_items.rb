3ffa029bc6599b392ff90daedf7ffeddb072c539
Ubuntu <ubuntu@ip-172-31-68-88.ec2.internal>
2020-11-02 19:07:59 +0000

    created models for user and item

---
+++ marketplaceApp/db/migrate/20201102190514_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.decimal :price, precision: 10, scale: 2
+      t.string :image
+      t.text :description
+      t.boolean :listed
+      t.string :status
+      t.boolean :deliverable
+      t.integer :seller
+
+      t.timestamps
+    end
+  end
+end
