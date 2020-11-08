41e73b493e0911b7b237ae22cefaa15206309d38
Ubuntu <ubuntu@ip-172-31-13-183.ec2.internal>
2020-11-08 02:22:41 +0000

    updated some rspec

--- marketplaceApp/spec/controllers/users_controller_spec.rb
+++ marketplaceApp/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,8 @@
-require 'users_controller'
-describe UsersController do
+require 'simplecov'
+SimpleCov.start
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
     it "creates a new user correctly" do
         x = User.create(name: 'allegra', email: 'aknox@colgate.edu', password: "password", payment: "allegra-knox")
         expect(x.name).to eq 'allegra'
@@ -7,4 +10,9 @@ describe UsersController do
         expect(x.password).to eq 'password'
         expect(x.payment).to eq 'allegra-knox'
     end
+  it 'should return true if new and save correctly' do
+    u = User.new :name=>"alle", :payment=>"venmo", :email=>"aknox@email.com", :password=>"password"
+    u.save
+    expect(User.new :name=>"alle", :payment=>"venmo", :email=>"aknox@email.com", :id=>1, :password=>"password").to eq u
+  end
 end
