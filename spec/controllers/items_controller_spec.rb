c49622e45eefc79da72957b34f71663bd48215ac
Ubuntu <ubuntu@ip-172-31-12-21.ec2.internal>
2020-11-09 04:56:15 +0000

    update items controller spec tests

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 require 'capybara/rails'
 
 RSpec.describe ItemsController, type: :controller do
+    before :each do
+        User.create!(name: "mariam", email: "mariam@colgate.edu", password: "colgate")
+    end
     context "root route" do
-        it "routes to items#index" do
+        it "routes to rental_properties#index" do
             expect(:get => '/').to route_to(:controller => 'items', :action => 'index')
         end
     end
@@ -15,48 +18,27 @@ RSpec.describe ItemsController, type: :controller do
         end
 
         it "renders the index template and does default sorting" do
-            x, y = Item.create(name: 'mask', price: 10.00, user_id: 1), Item.create(name: 'phone', price: 109.90, user_id: 1)
+            x, y = Item.create!(name: 'apple', price: 10, user_id: 1), Item.create!(name: 'zebra', price: 20, user_id: 1)
             get :index
-            # expect(page).to have content x
-            # expect(page).to_have content y
+            expect(assigns(:items)).to match_array([x,y])
             expect(response).to render_template("index")
         end
-        
-        it "should only show products that have been searched" do
-            ## need to set up capybara for this
-            
-            
-            #u = User.create(name: 'noahj', email: 'n@colgate.edu', password: "password", payment: "venmo" )
-            #i = u.items.create(name: "toy", price: 25.55, description: "Good toy")
-            #x, y = Item.create(name: 'mask', price: 10.00, user_id: 1), Item.create(name: 'phone', price: 109.90, user_id: 1)
-            #x, y = Item.create(name: 'toy', price: 10.00, user_id: 1), Item.create(name: 'books', price: 109.90, user_id: 1)
-            #names = []
-            #get :search, params: {:search => "mask"}
-            #page.all(".productname").each { |x| names << x.text }
-            #expect(names).to match_array(["mask"])
-         end
     end
 
     context "show" do
         it "routes correctly" do
-            p = Item.new(name: "shoes", user_id: 1)
-            u = User.new(name: "Mark")
+            p = Item.new(name: "Pwll o le", price: 10, user_id: 1)
             expect(Item).to receive(:find).with(eq("1").or eq(1)) { p }
-            expect(User).to receive(:find).with(eq("1").or eq(1)) { u }
             get :show, :params => { :id => 1 }
-            
             expect(response.status).to eq(200)
         end
 
         it "renders the show template" do
-             p = Item.new(name: "shoes", user_id: 1)
-            u = User.new(name: "Mark")
+            p = Item.new(name: "Pwll o le", price: 10, user_id: 1)
             expect(Item).to receive(:find).with(eq("1").or eq(1)) { p }
-            expect(User).to receive(:find).with(eq("1").or eq(1)) { u }
             get :show, :params => { :id => 1 }
-            
             expect(response).to render_template("show")
-            # expect(page).to have_content "shoes"
+            expect(assigns(:item)).to eq(p)
         end
     end
 end
