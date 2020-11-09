64db5aa2599b413b44526ebfa1b1321d9ce3d2af
Ubuntu <ubuntu@ip-172-31-92-112.ec2.internal>
2020-11-09 00:57:20 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :items, only: [:index, :show, :create, :new]
-    devise_for :users
+    devise_for :users, :path_prefix => 'd'
+    resources :users, :only=>[:show]
     root to: "items#index"
+    
+    get '/search' => 'items#search', :as => 'search_item'
 end
