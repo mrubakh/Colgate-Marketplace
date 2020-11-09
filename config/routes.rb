966270bbfea844794284c205c3f72a38140f7c33
Ubuntu <ubuntu@ip-172-31-12-21.ec2.internal>
2020-11-09 03:21:08 +0000

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :items, only: [:index, :show, :create, :new]
-    devise_for :users, :path_prefix => 'd'
+    devise_for :users
     resources :users, :only=>[:show]
     root to: "items#index"
     
