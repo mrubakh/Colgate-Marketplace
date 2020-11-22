5a3b2a3cb5895f8d03914b52b87f8277f67610d8
Ubuntu <ubuntu@ip-172-31-23-214.ec2.internal>
2020-11-22 20:14:13 +0000

    fixed the things reviewers suggested

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ get 'static_pages/about'
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :items, only: [:index, :show, :create, :new]
     devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
