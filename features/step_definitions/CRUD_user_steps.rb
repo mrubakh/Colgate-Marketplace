66c2afcbe387779f4f10ad8d6f7c1b0ea6ebd717
Ubuntu <ubuntu@ip-172-31-13-183.ec2.internal>
2020-12-02 18:26:28 +0000

    removed old CRUD steps from before SSO

--- features/step_definitions/CRUD_user_steps.rb
+++ features/step_definitions/CRUD_user_steps.rb
@@ -1,5 +1,3 @@
-require 'simplecov'
-
 Given('these Users:') do |table|
   # table is a Cucumber::MultilineArgument::DataTable
   table.hashes.each do |h|
@@ -8,10 +6,6 @@ Given('these Users:') do |table|
   end
 end
 Given('I am on the items index page') do
-  visit root_path
-end
-Given('I am on the items index page, not logged in') do
-  destroy_user_session_path
   visit root_path
 end
 Then('I should see {string}') do |string|
@@ -23,40 +17,7 @@ end
 When('I press {string}') do |string|
   page.click_on string
 end
-When('I login as {string} and {string}') do |string, string2|
-  page.fill_in "Email", :with => string
-  page.fill_in "Password", :with => string2
-  page.click_button "Log in"
-end 
 Then('I should be on the items index page') do
-  #index can be / or /products
+  #index can be / or /items
   expect(page).to have_current_path(/\/|\/items/)
-end
-Then('I should be on the create user page') do
-  expect(page).to have_current_path('/users/sign_up')
-end
-When('I fill in the following initializers:') do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  page.fill_in "Name", :with => "allegra"
-  page.fill_in "user_payment", :with => "venmo"
-  page.fill_in "Email", :with => "aknox@colgate.edu"
-  page.fill_in "Password", :with => "password"
-  page.fill_in "Password confirmation", :with => "password"
-end
-When('I fill in the following credentials:') do |table|
-  page.fill_in "Email", :with => "a@email.com"
-  page.fill_in "Password", :with => "password"
-end
-Then('I should be on the sign in page') do 
-  expect(page).to have_current_path("/users/sign_in")
-end 
-Then('I should be on the edit profile page') do
- expect(page).to have_current_path("/users/edit")
-end
-When('I edit my name and re-enter my password') do  |table|
-  page.fill_in "Name", :with => "Allegra Knox"
-  page.fill_in "user_current_password", :with => "password"
-end
-Given('I am on the new user page') do
-  visit new_user_session_path
 end
