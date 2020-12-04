5bcbe9dde57d7d35390e8d1bc6ed3aa1f7440564
Ubuntu <ubuntu@ip-172-31-13-183.ec2.internal>
2020-12-04 08:10:08 +0000

    user#show, items#index, items#show style and content changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
       @user = User.find(params[:id])
-      @faveitems = Item.all
+      @faveitems = Array.new
       @listeditems = Item.all.where("listed==?", true).where("user_id==?", @user.id)
       @unlisteditems = Item.all.where("listed==?", false).where("user_id==?", @user.id)
   end
