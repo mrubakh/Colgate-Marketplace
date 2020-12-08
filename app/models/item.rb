df6f233757b69a6d89ee007e2ad03b2ec01033fb
Mark Rubakh <mrubakh@colgate.edu>
2020-12-08 08:05:25 +0000

    rspec coverage -> 100% and imported autocomplete libraries

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,9 +8,17 @@ class Item < ApplicationRecord
     validates :name, :presence => true
     validates :price, :presence => true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
     validates :description, :presence => true, length: {maximum: 100}
-    validates :image, allow_blank: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
+    validate :image_type
     #size_range: 1..3.megabytes
     def self.search(query)
         Item.all.where("lower(name) LIKE ? OR category = ? OR lower(description) LIKE ?","%"+query+"%",Item.categories[query.capitalize],"%"+query+"%")
+    end
+    
+    private 
+    def image_type
+      return unless image.attached?
+      if !image.content_type.in?(%('image/jpeg image/png 'image/gif))
+          errors.add(:image, "needs to be a jpeg or png!")
+      end
     end
 end
